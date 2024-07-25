bac1d65b36151291e55fc0ed65677eb13a29878e
andreleo02 <andreleo02@gmail.com>
2024-07-25 18:07:49 +0200

    backend initial integration

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
+from blueprints.api import api
 
 app = Flask(__name__)
 
-@app.route("/api/hello-world")
-def hello_world():
-    return jsonify({"Hello": "World"})
+app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
