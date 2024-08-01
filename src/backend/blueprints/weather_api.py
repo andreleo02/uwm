5d2c52f31c9952f6b9f1d345d7e466cc8915d68c
andreleo02 <andreleo02@gmail.com>
2024-08-01 16:59:17 +0200

    added prettifier function for frontend

--- src/backend/blueprints/weather_api.py
+++ src/backend/blueprints/weather_api.py
@@ -1,37 +1,25 @@
 from flask import Blueprint, jsonify
 from utils.postgres_utils import get_weather_details
 from utils.redis_utils import get_all_weather, get_weather_status
+from utils.prettifier import prettify_weather_status, prettify_weather_statuses, prettify_weather_details
 
 weather_api = Blueprint('weather_api', __name__)
 
 @weather_api.route('', methods=['GET'])
 def get_weathers():
-    return jsonify(get_all_weather())
+    weather_statuses = get_all_weather()
+    result = prettify_weather_statuses(weather_statuses=weather_statuses)
+    return jsonify(result)
 
 @weather_api.route('/<string:dev_id>/status', methods=['GET'])
 def get_weather(dev_id: str):
-    return jsonify(get_weather_status(dev_id=dev_id))
+    weather_status = get_weather_status(dev_id=dev_id)
+    result = prettify_weather_status(weather_status=weather_status)
+    return jsonify(result)
 
 @weather_api.route('/<string:dev_id>/details', methods=['GET'])
 def get_weather_details_info(dev_id: str):
     weather_details = get_weather_details(dev_id=dev_id)
     weather_status = get_weather_status(dev_id=dev_id)
-    result = {}
-    if weather_details is not None and weather_status is not None:
-        result = {
-            'dev_id': dev_id,
-            'last_edit': weather_status['last_edit'],
-            'battery': weather_status['battery'],
-            'precipitation': weather_status['precipitation'],
-            'air_temp': weather_status['air_temp'],
-            'wind_speed': weather_status['wind_speed'],
-            'wind_direction': weather_status['wind_direction'],
-            'gust_speed': weather_status['gust_speed'],
-            'vapour_pressure': weather_status['vapour_pressure'],
-            'atmospheric_pressure': weather_status['atmospheric_pressure'],
-            'relative_humidity': weather_status['relative_humidity'],
-            'latitude': weather_details.latitude,
-            'longitude': weather_details.longitude,
-            'sensor_name': weather_details.sensor_name
-        }
+    result = prettify_weather_details(weather_status=weather_status, weather_details=weather_details)
     return jsonify(result)
